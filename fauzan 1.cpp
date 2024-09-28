dee4361fe4e2e8dee8fd383100da068bda8165b8
NauvalFauzan <nf7647415@gmail.com>
2024-09-28 11:51:17 +0700

    Add files via upload

---
+++ fauzan 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    
+    char nim[] = "2441076"; 
+    char nama[] = "Naufal Fauzan"; 
+    char jurusan[] = "Teknik Informatika"; 
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
+
